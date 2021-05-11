da1b9e06d6cba3b888b0e8bfda85da184db955ba
Andrey Gusev <gusevgaa@gmail.com>
2021-05-11 21:06:39 +0300

    refactor usersim

--- util/custom_transforms.py
+++ util/custom_transforms.py
@@ -3,29 +3,42 @@ import numpy as np
 
 
 class UserSimulator(object):
-    def __init__(self, clusters=10, maxLinesNumber=50):
+    def __init__(self, clusters=10, maxLinesNumber=50, imsize=256):
         self.clusters = clusters
         self.maxLinesNumber = maxLinesNumber
         self.angleScale = np.pi / 20
-        self.seed = 0
+        self.seed_number = 0
+        self.imsize = imsize
 
-    def __call__(self, img):
-        mask = np.full((img.shape[0], img.shape[1], 1), 0.0)
-        self.maxLength = mask.shape[0] / 4
+        self.numsamples = 1e6
+        self.imshape = np.array([self.imsize, self.imsize])
+
+        self.start_coords = np.random.normal(
+            loc=self.imshape / 2,
+            scale=self.imshape / 4,
+            size=(self.numsamples)
+        )
+        self.start_angles = np.random.uniform(2 * np.pi, size=self.numsamples)
+        self.numVs = np.random.randint(self.maxLinesNumber, size=self.numsamples)
+        self.lengths = np.random.uniform(self.maxLength, size=self.numsamples)
+        self.norm_deltas = np.random.normal(size=self.numsamples)
+        self.angle_deltas = np.random.normal(loc=0.0, scale=self.angleScale, size=self.numsamples)
+
+    def __call__(self):
+        mask = np.full((self.imsize, self.imsize, 1), 0.0)
+        self.maxLength = self.imsize / 4
 
         for i in range(self.clusters):
-            start_coord = np.random.normal(
-                loc=np.array(mask.shape[:2]) / 2, scale=np.array(mask.shape[:2]) / 4
-            )
+            start_coord = self.start_coords[self.seed]
             end_coord = np.zeros_like(start_coord)
-            startAngle = np.random.uniform(2 * np.pi)
+            startAngle = self.start_angles[self.seed]
 
-            numV = np.random.randint(self.maxLinesNumber)
-            length = np.random.uniform(self.maxLength)
+            numV = self.numVs[self.seed]
+            length = self.lengths[self.seed]
             for j in range(numV):
-                length += np.random.normal() * length / 10
+                length += self.norm_deltas[self.seed] * length / 10
 
-                angle = startAngle + np.random.normal(loc=0.0, scale=self.angleScale)
+                angle = startAngle + self.angle_deltas[self.seed]
 
                 if j % 2:
                     angle += np.pi
@@ -43,3 +56,9 @@ class UserSimulator(object):
                 start_coord = end_coord.copy()
         print("usersimulator called", start_coord, end_coord, length)
         return (mask == 0).astype(np.float32)
+    
+    @property
+    def seed(self):
+        self.seed_number += 1
+        return self.seed_number
+
